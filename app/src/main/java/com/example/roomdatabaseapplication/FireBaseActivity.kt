fafacf5d1aaab005385ad7e29772e9a647b68f23
savaisolanki <savai.solanki@brainvire.com>
2023-01-05 15:29:23 +0530

    Add One Xml-File and Remote Config

--- app/src/main/java/com/example/roomdatabaseapplication/FireBaseActivity.kt
+++ app/src/main/java/com/example/roomdatabaseapplication/FireBaseActivity.kt
@@ -1,11 +1,55 @@
 package com.example.roomdatabaseapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 
 class FireBaseActivity : AppCompatActivity() {
+    private lateinit var signIn: MaterialButton
+    private val remoteConfig = Firebase.remoteConfig
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fire_base)
+        signIn = findViewById(R.id.btn_signIn)
+
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+        remoteConf()
+
+
     }
+
+
+    fun remoteConf(){
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d(TAG, "Config params updated: $updated")
+                    val text = remoteConfig.getString("Login_Btn")
+                    signIn.text = text
+                    Toast.makeText(
+                        this, "Fetch and activate succeeded",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this, "Fetch failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+    }
+
 }
